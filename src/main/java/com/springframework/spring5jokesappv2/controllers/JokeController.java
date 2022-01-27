f3763d47b7d110b60a7b1ad5dd24a3ad1e907270
Vishal Thakkar <vishaljthakkar@gmail.com>
2022-01-26 22:46:40 -0800

    Add Joke Controller.closes $4.

---
+++ src/main/java/com/springframework/spring5jokesappv2/controllers/JokeController.java
@@ -0,0 +1,22 @@
+package com.springframework.spring5jokesappv2.controllers;
+
+import com.springframework.spring5jokesappv2.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+    private final JokeService jokeService;
+
+    // @Autowired => Not needed for constructor method of  DI.
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+        return "index";
+    }
+}
